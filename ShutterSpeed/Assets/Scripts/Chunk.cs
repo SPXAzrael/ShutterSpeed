cafacfb9dc7b06d4f993bfff6638702d88b57d79
Walcross <walcross1738@gmail.com>
2026-05-08 13:44:38 +0100

    Changes to powerup collection

--- ShutterSpeed/Assets/Scripts/Chunk.cs
+++ ShutterSpeed/Assets/Scripts/Chunk.cs
@@ -1,40 +1,49 @@
 using System.Collections.Generic;
-using Unity.Collections;
 using UnityEngine;
 
 public class Chunk : MonoBehaviour
 {
     [SerializeField] private GameObject fenceHazard;
-    [SerializeField] private GameObject powerupPrefab;
+<<<<<<< Updated upstream
+=======
+    [SerializeField] private GameObject[] powerup;
     [SerializeField] private GameObject coinPrefab;
 
+
+>>>>>>> Stashed changes
     [SerializeField] private float[] lanes = { -3f, 0f, 3f };
 
-    [SerializeField] float pickupSpawnChance = 0.3f;
-    [SerializeField] float pickupSpawnCoin = 0.5f;
-    [SerializeField] float coinSeperationValue = 2f;
-
-    List<int> availableLanes = new List<int>() { 0, 1, 2 };
     void Start()
     {
+<<<<<<< Updated upstream
+        SpawnFence();
+=======
         SpawnFences();
         SpawnPowerup();
         SpawnCoin();
+
+>>>>>>> Stashed changes
     }
 
     // Spawns between 0 - 2 fence obstacles every chunk
-    void SpawnFences()
+    void SpawnFence()
     {
+        List<int> availableLanes = new List<int>() { 0, 1, 2, };
         int fencesToSpawn = Random.Range(0, lanes.Length);
-        for (int i = 0; i < fencesToSpawn; i++)
+        for (int i = 0; i < fencesToSpawn; i++) 
         {
             if (availableLanes.Count <= 0) break;
-            int selectedLane = GetSelectedLane();
+
+            int randomLane = Random.Range(0, availableLanes.Count);
+            int selectedLane = availableLanes[randomLane];
+            availableLanes.RemoveAt(randomLane);
 
             Vector3 spawnPos = new Vector3(lanes[selectedLane], transform.position.y, transform.position.z);
             Instantiate(fenceHazard, spawnPos, Quaternion.identity, this.transform);
         }
     }
+<<<<<<< Updated upstream
+=======
 
     void SpawnPowerup()
     {
@@ -43,8 +52,12 @@ public class Chunk : MonoBehaviour
         int selectedLane = GetSelectedLane();
 
         Vector3 spawnPos = new Vector3(lanes[selectedLane], transform.position.y, transform.position.z);
-        Instantiate(powerupPrefab, spawnPos, Quaternion.identity, this.transform);
-    }
+
+        GameObject powerupToSpawn = powerup[Random.Range(0, powerup.Length)];
+        Instantiate(powerupToSpawn, spawnPos, Quaternion.identity, this.transform);
+
+    }    
+
     void SpawnCoin()
     {
         if (Random.value > pickupSpawnCoin || availableLanes.Count <= 0) return;
@@ -74,4 +87,5 @@ public class Chunk : MonoBehaviour
     }
 
 
+>>>>>>> Stashed changes
 }
