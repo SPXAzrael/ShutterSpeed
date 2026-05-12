5d136311c33131d340e07a6d0ac2aec8467e355b
Walcross <walcross1738@gmail.com>
2026-05-12 11:56:26 +0100

    Resolved additional merge errors

--- ShutterSpeed/Assets/Scripts/GameManager.cs
+++ ShutterSpeed/Assets/Scripts/GameManager.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] TMP_Text scoreText;
+    [SerializeField] TMP_Text distanceText;
     [SerializeField] float MagnetCooldown = 10f;
     [SerializeField] GameObject gameOverText;
     private float CooldownTimer = 0f;
 
     PlayerController player;
+    LevelGenerator levelGenerator;
 
     bool gameOver = false;
     public bool magnet = false;
@@ -34,6 +36,7 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         player = FindFirstObjectByType<PlayerController>();
+        levelGenerator = FindFirstObjectByType<LevelGenerator>();
     }
 
     private void Update()
@@ -46,7 +49,9 @@ public class GameManager : MonoBehaviour
                 magnet = false;
                 CooldownTimer = 0f;
             }
-        }    
+        }
+
+        DistanceTravelled();
     }
 
     public void ActivateDoubleCoins(float duration)
@@ -55,13 +60,6 @@ public class GameManager : MonoBehaviour
 
     }
 
-
-
-    public void ActivateDoubleCoins(float duration)
-    {
-        StartCoroutine(DoubleCoinsRoutine(duration));
-
-    }
 
     public void UpdateScore(int amount)
     {
@@ -83,6 +81,13 @@ public class GameManager : MonoBehaviour
         player.enabled = false;
         gameOverText.SetActive(true);
         Time.timeScale = 0.1f;
+    }
+    private void DistanceTravelled()
+    {
+        if (gameOver) return;
+
+        int distanceDisplay = Mathf.FloorToInt(levelGenerator.DistanceTravelled);
+        distanceText.text = distanceDisplay + " m";
     }
 
     IEnumerator DoubleCoinsRoutine(float duration)
